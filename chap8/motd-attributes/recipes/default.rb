c68ba37ae434eed6f212c44519ecf0a9f7a24143
aredmond <andrewpredmond@gmail.com>
2015-05-05 09:34:21 -0500

    Finished chapter 8

--- chap8/motd-attributes/recipes/default.rb
+++ chap8/motd-attributes/recipes/default.rb
@@ -15,7 +15,9 @@ pp node.debug_value('ipaddress')
 node.default['motd-attributes']['company'] = 'Mandrew Inc.'
 node.default['motd-attributes']['message'] = "It's a great day to be alive!"
 
+pp node.debug_value('motd-attributes', 'company')
 include_recipe 'motd-attributes::message'
+pp node.debug_value('motd-attributes', 'company')
 
 template '/etc/motd' do
     source 'motd.erb'
