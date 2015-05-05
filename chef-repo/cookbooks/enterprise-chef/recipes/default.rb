39d1ff5ab8ef6e220efcab124a4fd776cbb0ceaa
aredmond <andrewpredmond@gmail.com>
2015-05-05 14:10:13 -0500

    Location 3722

--- chef-repo/cookbooks/enterprise-chef/recipes/default.rb
+++ chef-repo/cookbooks/enterprise-chef/recipes/default.rb
@@ -5,5 +5,15 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 package_url = node['enterprise-chef']['url']
-package_name = ::File.basename(pachage_url)
-package_local_path = "#{chef::Config[:file_cache_path]}/#
+package_name = ::File.basename(package_url)
+package_local_path = "#{Chef::Config[:file_cache_path]}#{package_name}"
+
+#omnibus_package is remote (i.e., a URL) let's download it
+remote_file package_local_path do
+    source package_url
+end
+
+package package_local_path
+
+# reconfigure the installation
+execute 'chef-server-ctl reconfigure'
