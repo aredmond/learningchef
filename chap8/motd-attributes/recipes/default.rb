6977deb76c96de436f02f4c3424f9b88ed916f67
aredmond <andrewpredmond@gmail.com>
2015-05-05 09:23:37 -0500

    location 3451

--- chap8/motd-attributes/recipes/default.rb
+++ chap8/motd-attributes/recipes/default.rb
@@ -7,7 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 
+require 'pp'
+
+node.default['ipaddress'] = '1.1.1.1'
+pp node.debug_value('ipaddress')
+
+node.default['motd-attributes']['company'] = 'Mandrew Inc.'
 node.default['motd-attributes']['message'] = "It's a great day to be alive!"
+
+include_recipe 'motd-attributes::message'
 
 template '/etc/motd' do
     source 'motd.erb'
