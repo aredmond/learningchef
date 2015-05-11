ad1ebfc005eb510f17cdffd790b4348eb8d03b31
aredmond <andrewpredmond@gmail.com>
2015-05-11 05:11:20 -0500

    Finished Chapter 16

---
+++ chap15/chef-zero/cookbooks/apache/metadata.rb
@@ -0,0 +1,8 @@
+name             'apache'
+maintainer       'Andrew Redmond'
+maintainer_email 'myemail@email.com'
+license          'MIT'
+description      'Installs/Configures apache'
+long_description 'Installs/Configures apache'
+version          '0.2.0'
+
